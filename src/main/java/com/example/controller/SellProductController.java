3a0c3002521abbef0bea6dba1091038f8d72e32c
Mahfuj <bsse1505@iit.du.ac.bd>
2025-07-13 00:21:13 +0600

    a slight customization in the ui

---
+++ src/main/java/com/example/controller/SellProductController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import javafx.event.ActionEvent;
+
+public class SellProductController {
+
+    public void handleConfirmSale(ActionEvent actionEvent) {
+    }
+
+    public void handleSellProduct(ActionEvent actionEvent) {
+    }
+}
